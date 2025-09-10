8604f86137fa2029550b641d624e2ddc5fd7f7c6
Ghulam89 <gm6681328@gmail.com>
2025-09-10 08:00:06 -0700

    first commit

--- frontend/src/components/common/PageMetadata.jsx
+++ frontend/src/components/common/PageMetadata.jsx
@@ -75,14 +75,12 @@ const PageMetadata = ({
 
     return (
         <Helmet>
-            {/* Primary Meta Tags */}
-            <title>{title}</title>
+            {/* <title>{title}</title>
             <meta name="description" content={description} />
             {keywords && <meta name="keywords" content={keywords} />}
             {author && <meta name="author" content={author} />}
             <meta name="robots" content={robots} />
 
-            {/* Open Graph / Facebook */}
             <meta property="og:type" content="website" />
             {ogUrl && <meta property="og:url" content={ogUrl} />}
             <meta property="og:title" content={title} />
@@ -95,7 +93,6 @@ const PageMetadata = ({
             <meta property="og:locale" content="en_US" />
             {modifiedTime && <meta property="article:modified_time" content={modifiedTime} />}
 
-            {/* Twitter */}
             <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:title" content={title} />
             <meta name="twitter:description" content={description} />
@@ -103,10 +100,8 @@ const PageMetadata = ({
             <meta name="twitter:site" content="@UmbrellaPackaging" />
             <meta name="twitter:creator" content="@UmbrellaPackaging" />
 
-            {/* Canonical URL */}
             {canonicalUrl && <link rel="canonical" href={canonicalUrl} />}
             
-            {/* Structured Data */}
             {breadcrumbSchema && Object.keys(breadcrumbSchema).length > 0 && (
                 <script type="application/ld+json">
                     {JSON.stringify(breadcrumbSchema)}
@@ -131,7 +126,7 @@ const PageMetadata = ({
                 <script type="application/ld+json">
                     {JSON.stringify(faqItemSchema)}
                 </script>
-            )}
+            )} */}
         </Helmet>
     );
 };
